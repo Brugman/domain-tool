e3d5ff11b354ecd0cd3046bd3f9c897f908ff8fe
Tim Brugman <tim@timbrugman.com>
2019-12-17 17:03:44 +0100

    Fix: Env variables are strings.

--- app/functions/debug.php
+++ app/functions/debug.php
@@ -1,8 +1,13 @@
 <?php
 
-if ( getenv('APP_DEBUG') )
+if ( getenv('APP_DEBUG') == 'true' )
 {
     error_reporting( E_ALL );
     ini_set( 'display_errors', 'on' );
+}
+else
+{
+    error_reporting( 0 );
+    ini_set( 'display_errors', 'off' );
 }
 
