dd4d51c2ea644d8cac1e0b9e0343d479f24bba4a
Tim Brugman <tim@timbrugman.com>
2022-08-13 04:53:10 +0200

    Add debug if check
    Close #13

--- app/functions/debug.php
+++ app/functions/debug.php
@@ -5,7 +5,7 @@ if ( getenv('APP_DEBUG') == 'true' )
     error_reporting( E_ALL );
     ini_set( 'display_errors', 'on' );
 }
-else
+elseif ( getenv('APP_DEBUG') == 'false' )
 {
     error_reporting( 0 );
     ini_set( 'display_errors', 'off' );
